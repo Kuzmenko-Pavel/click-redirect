dae5b97de11130099203364f30b845e609aa78f1
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2018-02-01 03:51:09 +0200

    update adload

--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@
 import datetime
 import pymssql
 import uuid
+from time import sleep
 
 import dateutil.parser
 import pymongo
@@ -134,7 +135,9 @@ def addClick(offer_id, campaign_id, click_datetime=None, social=None, cost_perce
                 for row in cursor:
                     print row
                     click_cost = float(row.get('ClickCost', 0.0))
+                sleep(3)
                 connection_adload.commit()
+                sleep(3)
         except Exception as ex:
             # cursor.close()
             print ex
